68e6cd7c1c3e6a56a157caf27aaa9ca9c7b09a6e
Roberto Cesar Tavares <roberto.tavares@trt2.jus.br>
2022-12-06 18:05:15 -0300

    Limpa tela inicial do projeto e coloca título

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+/** Componente */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tour-of-heroes-frontend';
+  title = 'Tour of Heroes'; // title é uma variável
 }
